7f46b0b9dfd84e2e33cfce99dc290117cfb19845
Tomas Perez <tomasitopg6@gmail.com>
2020-06-19 03:07:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(array)
+  
+ array.sort do | a, b |
+   a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  
+  array.sort do | a, b |
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  
+  array.sort do | a, b |
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+
+  array.sort do | a, b |
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+  
+  array.sort do | a, b |
+    b <=> a[-1]
+  end
+end
+
+
+
+
 
